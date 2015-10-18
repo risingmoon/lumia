9328efca1f81f2e43ecda12b7ea9cd1e0118e8a6
Justin Lee <justindavidlee88@gmail.com>
2015-10-17 21:49:49 -0700

    Add enemy pathfinding ability
    * Add pathfinding library (EasyStar Plugin)

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ var map;
 var layer;
 var player;
 var cursors;
+var pathfinder;
+var test = true;
 
 function preload() {
   game.load.tilemap('map', 'level1.csv', null, Phaser.Tilemap.CSV);
@@ -23,6 +25,10 @@ function create() {
   layer = map.createLayer(0);
   map.setCollision(1);
 
+  var walkables = [0];
+  pathfinder = game.plugins.add(Phaser.Plugin.PathFinderPlugin);
+  pathfinder.setGrid(map.layers[0].data, walkables);
+
   //var rect = game.add.bitmapData(16,16);
   //rect.ctx.beginPath();
   //rect.ctx.rect(0,0,16, 16);
@@ -36,19 +42,43 @@ function create() {
   player.body.setSize(16,16);
   game.camera.follow(player);
 
-  //var enemy_bmd = game.add.bitmapData(16, 16);
-  //enemy_bmd.circle(8, 8, 8, "#008000");
-  //enemy = game.add.sprite(64, 64, enemy_bmd);
-  //game.physics.enable(enemy, Phaser.Physics.ARCADE);
-  //enemy.body.setSize(16, 16);
-
-  //Group creation
-  enemies = game.add.group();
   var enemy_bmd = game.add.bitmapData(16, 16);
   enemy_bmd.circle(8, 8, 8, "#008000");
-  enemies.create(32 * 14, 64, enemy_bmd);
+  enemy = game.add.sprite(32* 14, 64, enemy_bmd);
+  game.physics.enable(enemy, Phaser.Physics.ARCADE);
+  enemy.body.setSize(16, 16);
+
+  //Group creation
+  //enemies = game.add.group();
+  //var enemy_bmd = game.add.bitmapData(16, 16);
+  //enemy_bmd.circle(8, 8, 8, "#008000");
+  //enemies.create(32 * 14, 64, enemy_bmd);
 
   cursors = game.input.keyboard.createCursorKeys();
+}
+
+function findPathTo(tilex, tiley) {
+  //console.log("CALLED");
+  pathfinder.setCallbackFunction(function(path) {
+    path = path || [];
+    for(var i = 0, ilen= path.length; i < ilen; i++) {
+      //console.log(path[i].x, path[i].y);
+      //var tile = layer.getTileXY(path[i].x, path[i].y);
+      //console.log(tile);
+    }
+    blocked= false;
+    if (path.length>=1) {
+      enemy.x = path[1].x * 32;
+      enemy.y = path[1].y * 32;
+    }
+  });
+
+  //console.log(layer.getTileX(enemy.x), layer.getTileY(enemy.y));
+  pathfinder.preparePathCalculation(
+      [layer.getTileX(enemy.x), layer.getTileY(enemy.y) ],
+      [tilex, tiley]);
+  //pathfinder.preparePathCalculation([1,1 ], [3, 3]);
+  pathfinder.calculatePath();
 }
 
 
@@ -57,20 +87,20 @@ function update() {
 
   player.body.velocity.set(0);
 
-  if (cursors.left.isDown)
-  {
+  if (cursors.left.isDown) {
       player.body.velocity.x = -100;
   }
-  else if (cursors.right.isDown)
-  {
+  else if (cursors.right.isDown) {
       player.body.velocity.x = 100;
   }
-  else if (cursors.up.isDown)
-  {
+  else if (cursors.up.isDown) {
       player.body.velocity.y = -100;
   }
-  else if (cursors.down.isDown)
-  {
+  else if (cursors.down.isDown) {
       player.body.velocity.y = 100;
+  }
+  if ( test== true) {
+    findPathTo(layer.getTileX(player.x), layer.getTileY(player.y));
+    //test = false;
   }
 }
